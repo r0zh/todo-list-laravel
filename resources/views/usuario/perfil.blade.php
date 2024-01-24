b5784020364db3dd94d2ffc5bd6b2a4cc8e5cbe1
r0zh <r0zh017@proton.me>
2024-01-24 09:08:24 +0000

    Refactor user profile styling and color selection

--- resources/views/usuario/perfil.blade.php
+++ resources/views/usuario/perfil.blade.php
@@ -5,32 +5,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
     <title>Document</title>
 </head>
 
-<body @style(["background-color: {$fondo}" => true, "color: {$tinta}" => $tinta == 'white'])>
-    <h1> Perfil del usuario {{ $usuario->nombre }} </h1>
+<body @style(["background-color: {$fondo}" => true, "color: {$tinta}" => true])>
+    <h1 class="text-white font-bold text-5xl"> Perfil del usuario {{ $usuario->nombre }} </h1>
     <p> El color de fondo es {{ $fondo }} </p>
     <p> El color de la tinta es {{ $tinta }} </p>
     <p> La marca de tiempo es {{ time() }} </p>
     <form action="" method="get">
         <select name="fondo">
-            <option value="red">Rojo</option>
-            <option value="green">Verde</option>
-            <option value="blue">Azul</option>
-            <option value="yellow">Amarillo</option>
-            <option value="orange">Naranja</option>
-            <option value="purple">Morado</option>
-            <option value="pink">Rosa</option>
+            <option value="red" @selected($fondo == 'red')>Rojo</option>
+            <option value="green" @selected($fondo == 'green')>Verde</option>
+            <option value="blue" @selected($fondo == 'blue')>Azul</option>
+            <option value="yellow" @selected($fondo == 'yellow')>Amarillo</option>
+            <option value="orange" @selected($fondo == 'orange')>Naranja</option>
+            <option value="purple" @selected($fondo == 'purple')>Morado</option>
+            <option value="pink" @selected($fondo == 'pink')>Rosa</option>
         </select>
         <select name="tinta">
-            <option value="red">Rojo</option>
-            <option value="green">Verde</option>
-            <option value="blue">Azul</option>
-            <option value="yellow">Amarillo</option>
-            <option value="orange">Naranja</option>
-            <option value="purple">Morado</option>
-            <option value="pink">Rosa</option>
+            <option value="red" @selected($tinta == 'red')>Blanco</option>
+            <option value="green" @selected($tinta == 'green')>Verde</option>
+            <option value="blue" @selected($tinta == 'blue')>Azul</option>
+            <option value="yellow" @selected($tinta == 'yellow')>Amarillo</option>
+            <option value="orange" @selected($tinta == 'orange')>Naranja</option>
+            <option value="purple" @selected($tinta == 'purple')>Morado</option>
+            <option value="pink" @selected($tinta == 'pink')>Rosa</option>
         </select>
         <button>Enviar</button>
     </form>
